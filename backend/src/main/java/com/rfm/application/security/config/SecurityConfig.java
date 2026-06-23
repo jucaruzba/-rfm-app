38ff45d9ddca9bdd9b4056f2192ef20df66eb5e3
FABRICA <fabrica@fabrica.com>
2026-06-23 16:19:13 -0600

    ajustes

--- backend/src/main/java/com/rfm/application/security/config/SecurityConfig.java
+++ backend/src/main/java/com/rfm/application/security/config/SecurityConfig.java
@@ -21,7 +21,6 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;  // ← COR
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
